6e810f480f6720a33e58d56e550c26c1831ba71a
Linas Ziedas <linas@digitalmojo.com>
2013-05-29 16:08:36 +0300

    looking for solution

--- mr.php
+++ mr.php
@@ -1,5 +1,4 @@
 <?php
-
 $lines = array(
              'one two three four',
              'two three four',
@@ -7,12 +6,28 @@ $lines = array(
              'four one one',
              );
 
-$lineToWordCounts =
-    function($line) {
-        return array_count_values(explode(' ', $line));
-    };
+$lineToWordCounts = function($line) {
+    return array_count_values(explode(' ', $line));
+};
 
 
 $counts = array_map($lineToWordCounts, $lines);
 
+$sumWordCounts =
+    function($countsL, $countsR) {
+        // Get all the words
+        $words = array_merge(array_keys($countsL), array_keys($countsR));
+        $out = array();
+        // Put them in a new (Array: Word => Count)
+        foreach($words as $word) {
+            // Sum their counts
+            $out[$word] = isset($countsL[$word]) ? $countsL[$word] : 0;
+            $out[$word] += isset($countsR[$word]) ? $countsR[$word] : 0;
+        }
+        return $out;
+    };
+
+$totals = array_reduce($counts, $sumWordCounts, array());
 print_r($counts);
+var_export($totals);
+
--- mra.php
+++ mra.php
@@ -10,24 +10,46 @@ $lineToData = function($line) {
     return $item;
 };
 
+$sumWordCounts =
+    function($countsL, $countsR) {
+        // Get all the words
+        $words = array_merge(array_keys($countsL), array_keys($countsR));
+        $out = array();
+        // Put them in a new (Array: Word => Count)
+        foreach($words as $word) {
+            // Sum their counts
+            $out[$word] = isset($countsL[$word]) ? $countsL[$word] : 0;
+            $out[$word] += isset($countsR[$word]) ? $countsR[$word] : 0;
+        }
+        return $out;
+    };
+
+
 $sumDataCounts =
     function($countsL, $countsR) {
-        // var_dump($countsL);
-        // var_dump($countsR);
+        $auth = 'Michael Brady';
+        if (!in_array($auth, $countsR['authors'])) {
+            return $countsL;
+        }
 
-        // exit;
         foreach ($countsR['authors'] as $author) {
+            if ($author != $auth) {
+                continue;
+            }
+            $title = explode(' ', trim($countsR['title'], ";:,?!."));
             if (isset($countsL[$author])) {
-                $countsL[$author]['items'][] = array(
-                    'id' => $countsR['id'],
-                    'title' => $countsR['title']
-                );
+                // $countsL[$author]['items'][] = array(
+                //     'id' => $countsR['id'],
+                //     'title' => $countsR['title']
+                // );
+                $countsL[$author]['titles'] = array_count_values($title);
             } else {
-                $countsL[$author] = array('items'=>array());
-                $countsL[$author]['items'][] = array(
-                    'id' => $countsR['id'],
-                    'title' => $countsR['title']
-                );
+                // $countsL[$author] = array('items'=>array(), 'titles' => array());
+                // $countsL[$author]['items'][] = array(
+                //     'id' => $countsR['id'],
+                //     'title' => $countsR['title'],
+                // );
+                $countsL[$author]['titles'] = array_count_values($title);
             }
 
         }
@@ -36,8 +58,8 @@ $sumDataCounts =
     };
 
 $totals = array();
+
 foreach (glob($data) as $file) {
-    var_dump($file);
     $dataStream = explode("\n", trim(file_get_contents($file)));
     $dataConverted = array_map($lineToData, $dataStream);
     $totals = array_reduce($dataConverted, $sumDataCounts, $totals);
